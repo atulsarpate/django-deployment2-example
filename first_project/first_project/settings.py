45be57f63f247d97bbf88d6438072018d3e170ca
atulsarpate <87242684+atulsarpate@users.noreply.github.com>
2022-01-27 12:18:47 +0530

    Update settings.py

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -130,6 +130,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+BASEPATH = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = BASEPATH.joinpath('static')
+MEDIA_ROOT = BASEPATH.joinpath('media')
+
 # STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
 #
 # 
